be50cd9a92401651ef2ee7e867b71d4a3d14071f
Nikolai Calder <redlacialokin@gmail.com>
2015-12-23 07:38:42 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+# a = (1..999)
+# multiples = a.select {  |i| i % 3 == 0  || i % 5 == 0 }
+# multiples.reduce(:+)
+
+def collect_multiples(num)
+  collection = (1..num - 1).to_a
+  multiples = collection.select {  |i| i % 3 == 0  || i % 5 == 0 }
+end
+
+def sum_multiples(multiples)
+ list = collect_multiples(multiples)
+  list.reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,16 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit - 1
+  end
+
+  def collect_multiples
+   collection = (1..@limit).to_a
+   multiples = collection.select {  |i| i % 3 == 0  || i % 5 == 0 }
+  end
+
+  def sum_multiples
+    list = collect_multiples
+    list.reduce(:+)
+  end
+end
